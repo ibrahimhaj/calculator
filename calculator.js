7d06b364339dd3b852ef11d81d3daba75c669350
Shimitas <shmult@gmail.com>
2021-01-19 18:57:57 +0200

    change Function name

--- calculator.js
+++ calculator.js
@@ -2,9 +2,9 @@ function addNumbers(num1, num2) {
   return num1 + num2;
 }
 
-function multiFunction(num1, num2) {
+function multiplyFunction(num1, num2) {
   return num1 * num2;
 }
 
 addNumbers(2, 3);
-multiFunction(3, 3);
+multiplyFunction(3, 3);
